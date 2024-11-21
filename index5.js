a3109f5d04040de0719ce05aa0bfa482022e8813
htdeaf <165033671+htdeaf@users.noreply.github.com>
2024-11-21 15:51:57 +0100

    Add files via upload

---
+++ index5.js
@@ -0,0 +1,10 @@
+for(i=0;i<=30;i++){
+    if(i%3==!0){
+        console.log(i)
+    }
+    
+}
+let fruits=["bannana","fraise","poivre"]
+fruits.forEach(function(fruis){
+    console.log(fruis.toUpperCase())
+})
